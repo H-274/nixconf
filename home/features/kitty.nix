eacacaacff649dc2f23c9cedb2cac4c335d1d653
H-274 <gh@jsmarier.com>
2026-08-27 20:35:41 -0400

    chore: formatting

--- home/features/kitty.nix
+++ home/features/kitty.nix
@@ -1,11 +1,9 @@
-{
+{ pkgs, ... }: {
   programs.kitty = {
     enable = true;
     autoThemeFiles.noPreference = "Catppuccin-Mocha";
     autoThemeFiles.light = "Catppuccin-Latte";
     autoThemeFiles.dark = "Catppuccin-Mocha";
-    font = {
-      name = "nerd-fonts.atkynson-mono";
-    };
+    font.name = "nerd-fonts.atkynson-mono";
   };
 }
