dd93fe214837a5529e45aafd3bdf5f9f3a983d1d
H-274 <GitHub@jsmarier.com>
2026-08-30 14:14:21 -0400

    fix: fixed path

--- home/terminal/shell/omp.nix
+++ home/terminal/shell/omp.nix
@@ -1,4 +1,4 @@
 {
   programs.oh-my-posh.enable = true;
-  programs.oh-my-posh.configFile = ../../modules/oh-my-posh/zen.toml;
+  programs.oh-my-posh.configFile = ../../../modules/oh-my-posh/zen.toml;
 }
