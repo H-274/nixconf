1c9fff3446c7351cc826a8164ea58bf50f3cd0d9
H-274 <GitHub@jsmarier.com>
2026-08-31 21:25:47 -0400

    feat: added hyprland animations

--- home/hyprland/animations.lua
+++ home/hyprland/animations.lua
@@ -1,1 +1,73 @@
+-- From https://github.com/fufexan/dotfiles
+--
+----------------
+---- CURVES ----
+----------------
+hl.curve("slight_bounce", {
+    type = "spring",
+    mass = 1,
+    stiffness = 350,
+    dampening = 30
+})
 
+hl.curve("winIn", {
+    type = "spring",
+    mass = 1,
+    stiffness = 350,
+    dampening = 35
+})
+hl.curve("winOut", {
+    type = "spring",
+    mass = 1,
+    stiffness = 320,
+    dampening = 32
+})
+hl.curve("winMove", {
+    type = "spring",
+    mass = 1,
+    stiffness = 300,
+    dampening = 30
+})
+----------------------
+---- ANIMATIONS ------
+----------------------
+hl.animation({
+    leaf = "border",
+    enabled = true,
+    speed = 2,
+    bezier = "default"
+})
+hl.animation({
+    leaf = "fade",
+    enabled = true,
+    speed = 4,
+    bezier = "default"
+})
+hl.animation({
+    leaf = "workspaces",
+    enabled = true,
+    speed = 1,
+    spring = "slight_bounce"
+})
+
+hl.animation({
+    leaf = "windowsIn",
+    enabled = true,
+    speed = 3,
+    spring = "winIn",
+    style = "popin 85%"
+})
+hl.animation({
+    leaf = "windowsOut",
+    enabled = true,
+    speed = 3,
+    spring = "winOut",
+    style = "popin 85%"
+})
+hl.animation({
+    leaf = "windowsMove",
+    enabled = true,
+    speed = 3,
+    spring = "winMove",
+    style = "slide"
+})
