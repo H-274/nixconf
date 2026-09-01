50ce3c93b0644d1b46ce30475d4821151c70853b
H-274 <GitHub@jsmarier.com>
2026-08-31 22:35:02 -0400

    feat: removed kitty auto theme

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }: {
   programs.kitty = {
     enable = true;
-    autoThemeFiles.noPreference = "Catppuccin-Mocha";
-    autoThemeFiles.light = "Catppuccin-Latte";
-    autoThemeFiles.dark = "Catppuccin-Mocha";
     font = {
       name = "AtkynsonMono Nerd Font";
       package = pkgs.nerd-fonts.atkynson-mono;
