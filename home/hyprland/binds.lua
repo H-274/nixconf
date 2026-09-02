86a2462339e87778fbf54cf027b344345712d384
H-274 <GitHub@jsmarier.com>
2026-09-01 20:38:31 -0400

    feat: added back mouse binds

--- home/hyprland/binds.lua
+++ home/hyprland/binds.lua
@@ -6,6 +6,14 @@
 local mod = "SUPER"
 local ipc = "noctalia msg "
 
+-- mouse movements
+hl.bind(mod .. " + mouse:272", hl.dsp.window.drag(), {
+    mouse = true
+})
+hl.bind(mod .. " + mouse:273", hl.dsp.window.resize(), {
+    mouse = true
+})
+
 -- app shortcuts
 hl.bind(mod .. " + Return", hl.dsp.exec_cmd("kitty"))
 
