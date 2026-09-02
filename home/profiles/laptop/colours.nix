00e1924c43bf0d8e9ba312f7d908739505114398
H-274 <GitHub@jsmarier.com>
2026-09-01 22:15:55 -0400

    fix: extraConfig again

--- home/profiles/laptop/colours.nix
+++ home/profiles/laptop/colours.nix
@@ -29,7 +29,7 @@
     # Integrations
     programs.starship.enableFishIntegration = true;
     programs.kitty = {
-      extraConfig = "import themes/noctalia.conf";
+      extraConfig = "include themes/noctalia.conf";
       shellIntegration.enableFishIntegration = true;
     };
   };
